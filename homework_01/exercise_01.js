bc67ba250fe6133de033ad4b11cfc11fc61a17e8
Charles Muchene <charlesmuchene@gmail.com>
2019-04-01 18:35:31 -0500

    Refactors codebase

---
+++ homework_01/exercise_01.js
@@ -0,0 +1,57 @@
+// Filter words extension function
+String.prototype.filterWords = function(badWords) {
+	const delimiter = ' ';
+	return this.split(delimiter).map((word) => word.replace(new RegExp(badWords.join('|')), '***')).join(delimiter);
+};
+
+// *************************************************************** //
+
+// Exercise 01 part A
+const result = 'This house is nice!'.filterWords([ 'house', 'nice' ]);
+console.log(result);
+
+// *************************************************************** //
+
+// Exercise 01 part B
+function filterStringWordsB(statement, badWords) {
+	return new Promise(function(resolve, reject) {
+		const result = statement.filterWords(badWords);
+		resolve(result);
+	}).then(function(data) {
+		console.log(data);
+	});
+}
+
+filterStringWordsB('This house is nice!', [ 'house', 'nice' ]);
+
+// *************************************************************** //
+
+// Exercise 01 part C
+async function filterStringWordsC(statement, badWords) {
+	try {
+		const result = await statement.filterWords(badWords);
+		console.log(result);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+filterStringWordsC('This house is nice!', [ 'house', 'nice' ]);
+
+// *************************************************************** //
+
+// Exercise 01 part D
+const { Observable } = require('rxjs');
+
+function filterStringWordsD(statement, badWords) {
+	return new Observable(function(observer) {
+		const result = statement.filterWords(badWords);
+		observer.next(result);
+		observer.complete();
+	});
+}
+
+const observable = filterStringWordsD('This house is nice!', [ 'house', 'nice' ]);
+observable.subscribe((data) => {
+	console.log(data);
+});
--- homework_01/hw_01.js
+++
@@ -1,7 +0,0 @@
-String.prototype.filterWords = function(badWords) {
-	const delimiter = ' ';
-	return this.split(delimiter).map((word) => word.replace(new RegExp(badWords.join('|')), '***')).join(delimiter);
-};
-
-const result = 'This house is nice!'.filterWords([ 'house', 'nice' ]);
-console.log(result);
