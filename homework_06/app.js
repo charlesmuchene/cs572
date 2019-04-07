c2587fab79bfb15e17d3a636330217a857a1f806
Charles Muchene <charlesmuchene@gmail.com>
2019-04-07 17:58:01 -0500

    Implements logging with morgan

--- homework_06/app.js
+++ homework_06/app.js
@@ -1,5 +1,8 @@
 // Dependencies
+const fs = require('fs');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
 const gradesRouter = require('./routes/grades');
@@ -8,10 +11,12 @@ const indexRouter = require('./routes/index');
 // Setup
 const app = express();
 const port = 1234;
+const appLogStream = fs.createWriteStream(path.join(__dirname, 'log.log'), { flags: 'a' });
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('combined', { stream: appLogStream}));
 
 // Routes
 app.use('/', indexRouter);
