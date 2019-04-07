f80b557a302c3c1c96596cbd360f09a11173dfe2
Charles Muchene <charlesmuchene@gmail.com>
2019-04-07 17:36:33 -0500

    Implements grade delete route

--- homework_06/routes/grades.js
+++ homework_06/routes/grades.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 const grades = require('../data/grades');
 
+function gradeIdMiddleware(request, response, next) {
+	const id = request.params.id || -1;
+	const available = grades.find((grade) => grade.id == id);
+	if (!!available) next();
+	else next({ status: 404, message: `Grade with id ${id} is not found` });
+}
+
 router.get('/', (request, response, next) => response.send(grades));
 
-router.get('/:id', (request, response, next) => {
+router.get('/:id', gradeIdMiddleware, (request, response, next) => {
 	const id = request.params.id;
-	if (id > grades.length || id <= 0) return next({ status: 404, message: `No grade with id ${id}` });
 	const grade = grades[id - 1];
 	response.send(grade);
 });
@@ -16,6 +22,12 @@ router.post('/', (request, response, next) => {
 	const grade = request.body;
 	grade.id = grade.id || Math.ceil(Math.random() * 10 + grades.length);
 	grades.push(grade);
+	response.redirect('/api/grades');
+});
+
+router.delete('/:id', gradeIdMiddleware, (request, response, next) => {
+	const id = request.params.id;
+	grades.splice(id, 1);
 	response.redirect('/api/grades');
 });
 
