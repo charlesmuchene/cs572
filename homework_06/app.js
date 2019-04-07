5504c9da073903726c8e4a7d48494ee780dce3ba
Charles Muchene <charlesmuchene@gmail.com>
2019-04-07 17:39:02 -0500

    Add CORS

--- homework_06/app.js
+++ homework_06/app.js
@@ -1,4 +1,5 @@
 // Dependencies
+const cors = require('cors');
 const express = require('express');
 const createError = require('http-errors');
 const gradesRouter = require('./routes/grades');
@@ -9,6 +10,7 @@ const app = express();
 const port = 1234;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -17,14 +19,14 @@ app.use('/api/grades', gradesRouter);
 
 // 404
 app.use((request, response, next) => {
-    next(createError(404, `Couldn't find url ${request.path}`));
-})
+	next(createError(404, `Couldn't find url ${request.path}`));
+});
 
 // Errors
 app.use((error, request, response, next) => {
-    console.log(error);
-    response.status(error.status || 500);
-    response.send(`${error.status}: ${error.message}`)
-})
+	console.log(error);
+	response.status(error.status || 500);
+	response.send(`${error.status}: ${error.message}`);
+});
 
 app.listen(port, () => console.log('Started playground!'));
