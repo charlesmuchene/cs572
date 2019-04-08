774d10b872e7ce2bdc5f75ba596783e47467d973
Charles Muchene <charlesmuchene@gmail.com>
2019-04-08 17:11:03 -0500

    Wraps db connection with express request call

--- homework_07/exercise_02/app.js
+++ homework_07/exercise_02/app.js
@@ -3,36 +3,50 @@ const simpleEncryptor = require('simple-encryptor');
 const MongoClient = require('mongodb').MongoClient;
 
 const dbUri = 'mongodb://homework01:homework01@ds233806.mlab.com:33806/homework01';
-const client = new MongoClient(dbUri);
+const client = new MongoClient(dbUri, { useNewUrlParser: true });
 
-client.connect((error) => {
-	if (error) throw new Error(error.message);
-	const db = client.db('homework01');
-	const collection = db.collection('data');
-	collection
-		.findOne({}, { projection: { key: 1, message: 1, _id: 0 } })
-        .then((data) => {
-			client.close();
-            decodeMessage(data);
-		})
-		.catch((error) => {
-			console.log(error);
-			client.close();
-		});
+const app = express();
+const port = 1234;
+
+app.get('/', (request, response) => {
+	response.end('Be unbounded at /secret');
 });
 
-function decodeMessage(data) {
-    const { key, message } = data;
-    const encryptor = simpleEncryptor(key);
-    const secret = encryptor.decrypt(message);
-    // console.log(secret);
+app.get('/secret', (request, response) => {
+	fetchSecretMessage(response);
+});
+
+app.listen(port, () => console.log('Server started'));
+
+function fetchSecretMessage(response) {
+	client.connect((error) => dbConnectionHandler(error, response));
 }
-// const app = express();
-// const port = 1234;
 
-// app.get('/', (request, response) => {
-//     console.log("Do the necessary");
-//     response.end("Done");
-// })
+function dbConnectionHandler(error, response) {
+	if (error) {
+		console.log(error);
+		response.end(error.message);
+		return;
+	}
+	const db = client.db('homework01');
+	const collection = db.collection('data');
+	const projection = { projection: { key: 1, message: 1, _id: 0 } };
+	collection
+		.findOne({}, projection)
+		.then((data) => {
+			client.close();
+			const message = decodeMessage(data);
+			response.send({ message });
+		})
+		.catch((err) => {
+			client.close();
+			console.log(error);
+			response.end(error.message);
+		});
+}
 
-// app.listen(port, () => console.log('Server started'));
+function decodeMessage(data) {
+	const { key, message } = data;
+	const encryptor = simpleEncryptor(key);
+	return encryptor.decrypt(message);
+}
