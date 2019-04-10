1d3ac22d3f27f4c20a1f27457b74ebe0ca224b86
Charles Muchene <charlesmuchene@gmail.com>
2019-04-09 23:46:02 -0500

    Implemented a restuful express app
    Includes mongodb connection and has get and post endpoints

---
+++ homework_08/exercise_03/app.js
@@ -0,0 +1,54 @@
+// Dependencies
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+// Configuration
+const dbUri = 'mongodb://localhost:27017'
+const dbName = 'mum_locations';
+const port = 1234;
+const collectionName = 'locations';
+let database;
+
+// Initialization
+const app = express();
+const client = new MongoClient(dbUri);
+
+// Middlewares
+app.use(express.json())
+app.use((request, response, next) => {
+    if (!database) {
+        client.connect(error => {
+            database = client.db(dbName)
+            request.collection = database.collection(collectionName);
+            next();
+        })
+    } else {
+        request.collection = database.collection(collectionName);
+        next()
+    }
+})
+
+// Routes
+app.get('/locations', (request, response, next) => {
+    request.collection.find({}).toArray((error, data) => {
+        response.json({ data: data });
+    })
+})
+
+app.post('/locations', (request, response, next) => {
+    const location = request.body;
+    console.dir(location)
+    request.collection.insertOne(location);
+    response.json({status: 200, message: `Location inserted is ${location}`})
+})
+
+// Error handling
+
+
+// Bootup
+app.listen(port, () => console.log("Shikorina :3"))
+
+process.on('exit', () => {
+    console.log("We are closing down the server")
+    client.close();
+})
