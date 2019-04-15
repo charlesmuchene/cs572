0d39fb7c781681baf5014056b5598be5a926ae45
Charles Muchene <charlesmuchene@gmail.com>
2019-04-15 14:56:01 -0500

    Implements counter variable increment/decrement actions

--- homework_12/src/app/counter.component.ts
+++ homework_12/src/app/counter.component.ts
@@ -4,15 +4,24 @@ import { Component, OnInit } from '@angular/core';
 	selector: 'app-counter',
 	template: `
     <div>
-      <button>-</button>{{counterValue}}<button>+</button>
+      <button (click)="decrement()">-</button>  {{counterValue}}  <button (click)="increment()">+</button>
     </div>
   `,
 	styles: [ `div {text-align: center;}` ]
 })
 export class CounterComponent implements OnInit {
-	counterValue: 0;
+	counterValue: number = 0;
 
 	constructor() {}
 
 	ngOnInit() {}
+
+	decrement() {
+		this.counterValue--;
+	}
+
+	increment() {
+		this.counterValue++;
+  }
+  
 }
