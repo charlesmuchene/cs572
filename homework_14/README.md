c442b0d0fa73a8347e52589f4f06b5edea9c87fe
Charles Muchene <charlesmuchene@gmail.com>
2019-04-17 13:45:03 -0500

    Commit ya kwanza

---
+++ homework_14/README.md
@@ -0,0 +1,7 @@
+# Homework14
+
+Homework 14
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`.
---
+++ homework_14/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	template: `
+    We are one
+  `,
+	styles: []
+})
+export class AppComponent {
+	title = 'homework14';
+}
