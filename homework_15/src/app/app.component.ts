1cf13911533fe1fe1c38017171a11856798947d1
Charles Muchene <charlesmuchene@gmail.com>
2019-04-19 21:24:50 -0500

    Finished works

--- homework_15/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-root',
-	templateUrl: './app.template.html',
-	styles: [ `main { width: 40%; margin: auto}` ]
-})
-export class AppComponent {}
